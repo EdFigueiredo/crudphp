2cd75247c5d0e1e4175341119dd5ab5611d982c5
Edson Figueiredo <86436713+EdFigueiredo@users.noreply.github.com>
2021-09-15 14:40:50 -0300

    Update processa_matricula.php

--- processa_matricula.php
+++ processa_matricula.php
@@ -3,7 +3,6 @@ include ('conecta.php');
 $id_aluno = $_POST['escolha_aluno'];
 $id_curso = $_POST['escolha_curso'];
 $query = "INSERT INTO ALUNOS_CURSOS(id_aluno, id_curso) VALUES ($id_aluno, $id_curso)";
-//Aqui não precisamos colocar aspas nas variáveis que irão receber os dados do banco por conta que é do tipo inteiro.
 mysqli_query($conexao, $query);
 
 header('location:index.php?pagina=matriculas');
